ba343b8d753969e286f27e2ccd110fb5fcf807c1
Bruno Rocha <rochacbruno@gmail.com>
2014-06-19 21:28:38 -0300

    fix tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.abspath(__name__))
 
 class BasicTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = create_app(config_filename="settings.py")
+        self.app = create_app(mode="development")
 
     def test_request_args(self):
         with self.app.test_request_context('/?name=BrunoRocha'):
